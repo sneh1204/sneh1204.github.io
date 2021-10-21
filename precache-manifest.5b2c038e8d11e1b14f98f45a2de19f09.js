055fe12e4ac814fa3b16c38b36eddcc3dfc39f63
Infernus <Infernus101@users.noreply.github.com>
2021-10-20 22:49:30 -0400

    init react

---
+++ precache-manifest.5b2c038e8d11e1b14f98f45a2de19f09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8956ffc454d3f9f60005805259bb64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5de618ca141cd8325dc7",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "c6b47daaacefce5298a3",
+    "url": "/static/js/2.7303e263.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.7303e263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de618ca141cd8325dc7",
+    "url": "/static/js/main.572ef6a5.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "9b98d31625fbff291331657e9f2157f2",
+    "url": "/static/media/pfp.jpg"
+  },
+  {
+    "revision": "fcf0be9bf946862ea58632d85350be81",
+    "url": "/static/media/Resume.pdf"
+  }
+]);
